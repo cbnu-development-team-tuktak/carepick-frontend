034f0263c618eb6a3ff70336d997fe10c6e1b8fa
mega-317 <mega31712@gmail.com>
2025-04-06 05:32:42 +0900

    <자가진단 뷰> 입력창을 제공하고, 입력창은 키보드가 올라오면 자동으로 올라간다. 텍스트를 입력하고 전송 버튼을 클릭하면 사용자 메시지가 화면에 출력된다

--- app/src/main/java/com/example/carepick/ui/selfcheck/SelfCheckFragment.kt
+++ app/src/main/java/com/example/carepick/ui/selfcheck/SelfCheckFragment.kt
@@ -4,7 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowManager
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import com.example.carepick.R
 import com.example.carepick.databinding.FragmentSelfCheckBinding
 
 // 아직 구현을 시작하지 않음
@@ -17,11 +20,40 @@ class SelfCheckFragment: Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentSelfCheckBinding.inflate(inflater, container, false)
+
+        binding.selfCheckSendButton.setOnClickListener() {
+            val inputText = binding.selfCheckInput.text.toString()
+            if (inputText.isNotBlank()) {
+                val inflater = LayoutInflater.from(requireContext())
+                val messageView = inflater.inflate(R.layout.item_message, binding.messageList, false)
+
+                val textView = messageView.findViewById<TextView>(R.id.messageText)
+                textView.text = inputText
+
+                binding.messageList.addView(messageView)
+                binding.selfCheckInput.text?.clear()
+            }
+
+            binding.selfCheckScrollView.post {
+                binding.selfCheckScrollView.fullScroll(View.FOCUS_DOWN)
+            }
+        }
+
         return binding.root
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    override fun onResume() {
+        super.onResume()
+        requireActivity().window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        requireActivity().window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_NOTHING)
     }
 }
