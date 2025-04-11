78b90aaf46b9150e21749a7c47b810f14a8ba13e
mega-317 <mega31712@gmail.com>
2025-04-11 18:39:29 +0900

    <하단 네비게이션 바> 네비게이션 바 구현 완료

--- app/src/main/java/com/example/carepick/MainActivity.kt
+++ app/src/main/java/com/example/carepick/MainActivity.kt
@@ -3,8 +3,13 @@ package com.example.carepick
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
+import com.example.carepick.ui.favorite.FavoriteFragment
 import com.example.carepick.ui.home.HomeFragment // MainActivity가 로드할 Fragment
+import com.example.carepick.ui.profile.UserProfileFragment
+import com.example.carepick.ui.search.SearchResultFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 
 class MainActivity : AppCompatActivity() {
@@ -14,16 +19,34 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
-//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.fragment_container)) { v, insets ->
-//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-//            insets
-//        }
-
         // 메인 액티비티가 ui를 가질 경우 특정 프래그먼트를 불러올 때 화면이 겹치는 문제가 발생한다
         // 따라서 메인 액티비티는 자체 ui를 가지지 않고 바로 HomeFragment를 불러오도록 하였다
         if (savedInstanceState == null) {
             loadFragment(HomeFragment())
+        }
+
+        findViewById<ConstraintLayout>(R.id.nav_home).setOnClickListener {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, HomeFragment())
+                .commit()
+        }
+
+        findViewById<ConstraintLayout>(R.id.nav_search).setOnClickListener {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, SearchResultFragment())
+                .commit()
+        }
+
+        findViewById<ConstraintLayout>(R.id.nav_favorite).setOnClickListener {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, FavoriteFragment())
+                .commit()
+        }
+
+        findViewById<ConstraintLayout>(R.id.nav_profile).setOnClickListener {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, UserProfileFragment())
+                .commit()
         }
     }
 
